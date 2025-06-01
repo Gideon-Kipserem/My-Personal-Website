caa5b6933d6957f5d4445c50f491878dc69ecb70
Gideon <gideonkimaiyo254@gmail.com>
2025-06-01 14:34:02 +0300

    My first website

---
+++ script.js
@@ -0,0 +1,17 @@
+const number = document.getElementById("number");
+
+function changeColor(event){
+    event.target.style.backgroundColor = "gray";
+}
+number.addEventListener("mouseover", changeColor);
+
+
+const mail = document.getElementById("mail");
+
+function changeColor(event){
+    event.target.style.backgroundColor = "gray";
+}
+number.addEventListener("mouseover", changeColor);
+
+
+
